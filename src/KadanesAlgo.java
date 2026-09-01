03eb6c2afdefa325e45009e76aec016a1a427acd
Nishant Kumar <nishantsingh50975@gmail.com>
2026-09-01 19:12:08 +0530

    kadane's modification

--- src/KadanesAlgo.java
+++ src/KadanesAlgo.java
@@ -7,17 +7,28 @@ public class KadanesAlgo {
         int n=sc.nextInt();
         int[] arr=new int[n];
         for(int i=0;i<n;i++)arr[i]=sc.nextInt();
-
+        int start=0;
+        int i_start=0;
+        int j_start=0;
         int sum=0;
         int max_sum_till_now=Integer.MIN_VALUE;
 
         for(int i=0;i<n;i++){
             sum+=arr[i];
-            max_sum_till_now=Math.max(max_sum_till_now,sum);
+            if(max_sum_till_now<sum){
+                max_sum_till_now=sum;
+                i_start=start;
+                j_start=i;
+            }
+//            max_sum_till_now=Math.max(max_sum_till_now,sum);
             if(sum<0){
                 sum=0;
+                start=i+1;
             }
         }
         System.out.println(max_sum_till_now);
+        for(int i=i_start;i<=j_start;i++){
+            System.out.print(arr[i]+" ");
+        }
     }
 }
